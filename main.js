d35d6e186463bd170154acef3c63f720c6ddac10
luixal <luixal@gmail.com>
2022-08-23 13:11:19 +0200

    Adds hidding/showing past/future days

--- main.js
+++ main.js
@@ -3,20 +3,33 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      currentWeekDayIndex: (new Date()).getDay() - 1
+      currentWeekDayIndex: (new Date()).getDay() - 1,
+      hidePast: false,
+      hideFuture: false
     }
   },
 
   computed: {
     items() {
-      console.log(data);
+      if (this.hidePast && this.hideFuture) return data.slice(this.currentWeekDayIndex, this.currentWeekDayIndex + 1);
+      if (this.hidePast) return data.slice(this.currentWeekDayIndex, 5);
+      if (this.hideFuture) return data.slice(0, this.currentWeekDayIndex + 1);
       return data;
+    },
+    getGridClass() {
+      return `md:grid-cols-${this.items.length}`;
     }
   },
 
   methods: {
     isCurrentWeekDay(index) {
-      return index === this.currentWeekDayIndex;
+      return this.hidePast ? index === 0 : index === this.currentWeekDayIndex;
+    },
+    toggleHidePast() {
+      this.hidePast = !this.hidePast;
+    },
+    toggleHideFuture() {
+      this.hideFuture = !this.hideFuture;
     }
   }
 }).mount('#app');
